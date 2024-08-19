6282a06c07f19ef0b8d4dde50678a6ab1a039e74
Ethan Jefferson <ethanjefferson14@gmail.com>
2024-08-19 10:04:52 -0700

    added comments

--- get_package_version_list.R
+++ get_package_version_list.R
@@ -1,36 +1,19 @@
-<<<<<<< HEAD
 library(git2r)
 library(reprex)
 
 get_package_version_list <- function(repo_path) {
   
+  #check to see if repo path exists
   if (!dir.exists(repo_path)) {
     stop("The system cannot find the path specified")
   }
   
-  repo <- repository("path/to/your/repo")
+  #checks the repo
+  repo <- repository(repo_path)
   commit_list <- commits(repo)
   
   commit_hashes <- sapply(commit_list, function(commit) commit$sha)
   
-  #return data.frame
+  #return list of comit hashes
   return(commit_hashes)
-=======
-library(git2r)
-library(reprex)
-
-get_package_version_list <- function(repo_path) {
-  
-  if (!dir.exists(repo_path)) {
-    stop("The system cannot find the path specified")
-  }
-  
-  repo <- repository("path/to/your/repo")
-  commit_list <- commits(repo)
-  
-  commit_hashes <- sapply(commit_list, function(commit) commit$sha)
-  
-  #return data.frame
-  return(commit_hashes)
->>>>>>> 2faf5b3e380c8bc3aa70444d27e96dad7849d24c
 }
