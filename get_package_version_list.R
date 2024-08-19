6f164c442388b14eab13de5632346fd1a86a5ca8
Ethan Jefferson <ethanjefferson14@gmail.com>
2024-08-19 13:57:30 -0700

    added comments

--- get_package_version_list.R
+++ get_package_version_list.R
@@ -3,13 +3,16 @@ library(reprex)
 
 get_package_version_list <- function(repo_path) {
 
+  #checks to see if the repo_path exists
   if (!dir.exists(repo_path)) {
     stop("The system cannot find the path specified")
   }
   
+  #opens the repository and gets every commit
   repo <- repository(repo_path)
   commit_list <- commits(repo)
   
+  #takes all the hashes from the commit_list
   commit_hashes <- sapply(commit_list, function(commit) commit$sha)
   
   
@@ -18,3 +21,4 @@ get_package_version_list <- function(repo_path) {
   
   
 }
+
